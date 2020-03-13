5003e75992821633e2f4bff8670240a77c59e7b3
NewDivide41194 <heinhtet25944@gmail.com>
2020-03-13 11:25:17 +0630

    nav fix

--- src/features/app/NavBar.js
+++ src/features/app/NavBar.js
@@ -3,7 +3,7 @@ import Logo from "../../assets/images/Logo.jpg";
 
 const NavBar = () => {
   return (
-    <div className="d-flex flex-row py-3 px-4 fixed-top bg-light">
+    <div className="d-flex flex-row py-3 px-4 sticky-top bg-light">
       <img src={Logo} style={{ width: "150px", height: "30px" }} alt="logo" />
     </div>
   );
