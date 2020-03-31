53c1df7aa2bea7ccb1b6952d5dcf07ce2e849c4f
Kumo <hsumyatmon.ntu@gmail.com>
2020-03-31 16:36:16 +0630

    ppm

--- src/pages/building/components/Building.js
+++ src/pages/building/components/Building.js
@@ -6,47 +6,63 @@ import { Link } from "react-router-dom";
 import * as Colors from "../../../config/Color.config";
 
 const Building=props=>{
-    return(
-        <div className="row justify-content-center">
-           <form className="" style={{
-               margin: 0,
-               position: "absolute",
-               top: '20%'
-               }}>
-               <div className="">
-                <div className=" font-weight-bold" style={{ fontSize: "25px" }}>
-                   Select Building for Survey
-                </div>
-                <div className="py-4 row form-group">
-                    <div className="py-2 col-5">
-                        Building Name:
-                    </div>
-                    <div className="py-2 col-7">
-                    <ESInput/>
-                    </div>
-                </div>
-                <div className="py-2 row form-group">
-                    <div className=" col-5">
-                       Postal:
-                    </div>
-                    <div className="col-7">
-                         <ESInput
-                        />
-                    </div>
-                </div>
-                <div className="py-2 row form-group">
-                    <div className=" col-5">
-                      Address:
-                    </div>
-                    <div className="col-7">
-                         <ESInput
-                        />
-                    </div>
-                </div>
 
-               </div>
-           </form>
+    return(
+        <div className="container">
+            <div className="row justify-content-center">
+            <form className="" style={{
+                margin: 0,
+                position: "absolute",
+                top: '30%'
+                }}>
+                <div className="">
+                    <div className="font-weight-bold" style={{ fontSize: "25px" }}>
+                        Select Building for Survey
+                    </div>
+                    <div className="py-4 row form-group">
+                        <div className="col-lg-5 col-sm-12">
+                            Building Name:
+                        </div>
+                        <div className="col-lg-7 col-sm-12" >
+                        <ESInput
+                        id= {"buildingName"}/>
+                        </div>
+                    </div>
+                    <div className="py-2 row form-group">
+                        <div className="col-lg-5 col-sm-12">
+                        Postal:
+                        </div>
+                        <div className="col-lg-7 col-sm-12">
+                            <ESInput
+                            id={"postal"}
+                            />
+                        </div>
+                    </div>
+                    <div className="py-2 row form-group">
+                        <div className="col-lg-5 col-sm-12">
+                        Address:
+                        </div>
+                        <div className="col-lg-7 col-sm-12">
+                            <ESInput
+                            id={"address"}
+                            />
+                        </div>
+                    </div>
+                    <div className="py-2 row form-group">
+                        <div className="col-lg-5 col-sm-12">
+                        Comment:
+                        </div>
+                        <div className="col-lg-7 col-sm-12">
+                            <ESInput
+                            id={"comment"}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </form>
+            </div>
         </div>
+    
     )
 }
 export default Building;
