56db5c56efd9dbecb7aec7c024896ccbe2afb33f
NewDivide41194 <heinhtet25944@gmail.com>
2020-04-01 14:39:35 +0630

    hh

--- src/tools/ES_TextField.js
+++ src/tools/ES_TextField.js
@@ -3,10 +3,51 @@ import * as Color from '../config/Color.config';
 
 export const ESTextfield=props=>{
     const {
-        style, 
-    }=props
+        style,
+        className,
+        placeHolder,
+        required,
+        type,
+        id,
+        height,
+        width,
+        onChange,
+        value,
+        maxLength
+    }=props;
 
-}
+    const defaultStyle={
+        width: width ===undefined ? "100%"
+    };
+
+    const userStyle=style ===undefined ? {} :style;
+
+    const_handleFocus =()=>{
+        document.getElementById(
+            id
+        ).style.border= `1px solid $`
+    }
+
+    return(
+        <input 
+        autoComplete="off"
+        spellCheck="false"
+        id={id}
+        required={required}
+        onChange={onChange}
+        style={{
+
+        }}  
+        placeholder={placeHolder}
+        className={`form-control form-rounded ${className}`}
+        onFocus={_handleFocus}
+        onBlur={_handleBlur}
+        type={type ===undefined ? "text" `${type}`}
+        value={value}
+        maxLength={maxLength}      
+        />
+    );
+};
 
 
 
