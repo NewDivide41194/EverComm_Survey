c9d2b44d023a9752b2a88bed823d79bddca3067b
NewDivide <heinhtet41194@gmail.com>
2020-04-19 16:13:33 +0630

    report back to menu

--- src/pages/Report/Components/Report.js
+++ src/pages/Report/Components/Report.js
@@ -7,14 +7,16 @@ const Report = props => {
   const { ReportData } = props;
   const surveyTitle = ReportData.length && ReportData[0].survery_title;
   const pageNo = ReportData.length && ReportData[0].survey_sections.length;
+  const buildingId=localStorage.getItem("buildingId")
+  const userId=JSON.parse(localStorage.getItem("userData"))[0].login_user_id
+  const surveyHeaderId=localStorage.getItem("SurveyHeaderId")
+
   const _handleMenu = () => {
-    props.history.push("/menu");
+    props.history.push(`/menu/${userId}`);
   };
 
   const _handleReview = () => {
-    const buildingId=localStorage.getItem("buildingId")
-    const userId=JSON.parse(localStorage.getItem("userData"))[0].login_user_id
-    const surveyHeaderId=localStorage.getItem("SurveyHeaderId")
+  
     props.history.push(`/question/${userId}/${surveyHeaderId}/${buildingId}`);
   };
   return (
