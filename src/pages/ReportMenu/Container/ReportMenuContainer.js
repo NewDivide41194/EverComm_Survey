8971cc020d36ae892201177951085cb8b6f6afe0
pyaemon <pyaephyomon114@gmail.com>
2020-05-20 12:19:38 +0630

    select surveyName@ppm

--- src/pages/ReportMenu/Container/ReportMenuContainer.js
+++ src/pages/ReportMenu/Container/ReportMenuContainer.js
@@ -1,8 +1,13 @@
-import React, { useState } from "react";
+import React, { useState,useEffect} from "react";
 import ReportMenu from "../Components/ReportMenu";
+import { MenuInfoFetch } from "../../../api/FetchMenuInfo";
 import moment from "moment";
 
 const ReportMenuContainer = (props) => {
+  const[surveyId,setSurveyId]=useState("");
+  const [menuData, setMenuData] = useState([]);
+  const userId = localStorage.getItem("userId")
+  const token=localStorage.getItem("token")
   const [startDate, setStartDate] = useState(null);
   const [endDate, setEndDate] = useState(null);
   const [focusedInput, setFocusedInput] = useState(null);
@@ -15,7 +20,25 @@ const ReportMenuContainer = (props) => {
       window.alert("Date Not Selected");
     }
   };
+  useEffect(()=>{
+    MenuInfoFetch({userId,token},(err,data)=>{
+      setMenuData(data.payload);
+    })
+  },[]);
+  console.log("menu data....",menuData);
 
+  const SurrveyNameOptions = menuData.map((v, k) => ({
+    value: v.survey_header_id,
+    label: v.survey_name,
+  }));
+  
+  const _handleSelectSurvey = (SurveyHeaderId, e) => {
+    localStorage.setItem("SurveyHeaderId", e.value);
+    setSurveyId(e.value);
+    console.log("surveyheaderid",e.value);
+  };
+ 
+  
   const _handleDatesChange = ({ startDate, endDate }) => {
     setStartDate(startDate);
     setEndDate(endDate);
@@ -25,7 +48,10 @@ const ReportMenuContainer = (props) => {
     <ReportMenu
       startDate={startDate}
       endDate={endDate}
+      SurrveyNameOptions={SurrveyNameOptions}
       focusedInput={focusedInput}
+      surveyId={surveyId}
+      _handleSelectSurvey={_handleSelectSurvey}
       _handleDatesChange={_handleDatesChange}
       _handleFocusedInput={_handleFocusedInput}
       _handleReport={_handleReport}
