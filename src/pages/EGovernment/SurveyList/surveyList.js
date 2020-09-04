f17b6f5488eb4731a9cc96c67b95f2a3b505c350
NewDivide41194 <heinhtet25944@gmail.com>
2020-09-04 08:53:08 +0630

    pull

---
+++ src/pages/EGovernment/SurveyList/surveyList.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { withRouter } from "react-router-dom";
+import withMedia from "react-media-query-hoc/dist/with-media";
+
+const Surveylist = (props) => {
+  const {
+    progress,
+    buildingName,
+    buildingType,
+    BgColor,
+    TxtColor,
+    HoverBgColor,
+    SurveyList,
+    Country,
+    id,
+  } = props;
+  const _handleMouseOver = (e, buildingName) => {
+    localStorage.setItem("buildingName", buildingName);
+    localStorage.setItem("buildingType", buildingType);
+    localStorage.setItem("bTypeId", bTypeId);
+
+    localStorage.setItem("buildingId", e.target.id);
+    const Card = document.getElementById(id);
+    Card.className =
+      "d-flex flex-row p-3 rounded justify-content-between my-2 shadow-sm";
+    Card.style.background = HoverBgColor;
+  };
+
+  const _handleMouseLeave = () => {
+    const Card = document.getElementById(id);
+    Card.className = "d-flex flex-row p-3 rounded justify-content-between my-2";
+    Card.style.background = BgColor;
+    Card.style.color = TxtColor;
+  };
+
+  return (
+    <div
+      className="d-flex flex-row p-3 rounded justify-content-between my-2"
+      id={id}
+      style={{
+        background: BgColor,
+        color: TxtColor,
+        cursor: "pointer",
+      }}
+      onMouseOver={(e) => _handleMouseOver(e, buildingName)}
+      onMouseLeave={_handleMouseLeave}
+      onClick={(e) => handleCardClick(e)}
+    >
+      <div
+        id={id}
+        style={{
+          fontSize: "22px",
+          // fontWeight:"bold",
+        }}
+      >
+        {SurveyList}{" "}
+        <span id={id} style={{ fontSize: "15px" }}>
+          ({Country})
+        </span>
+      </div>
+      <span id={id}> {progress}</span>
+    </div>
+  );
+};
+export default withRouter(withMedia(Surveylist));
